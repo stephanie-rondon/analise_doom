04cf0080062d19d643675a227b214abeb370a6c4
stephanie-rondon <stephanie.rondon@escola.pr.gov.br>
2025-07-03 08:53:51 -0300

    Adição do css e do dashboard

--- doom/dashboard.php
+++ doom/dashboard.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="doom\style.css">
+    <link rel="stylesheet" href="doom\header.css">
+    <title>Dashboard</title>
+</head>
+<body>
 <?php
 session_start();
 
@@ -6,13 +17,52 @@ if(!isset($_SESSION['usuario'])) {
     exit();
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Dashboard</title>
-</head>
-<body>
-    <h1>Bem-vindo, <?php echo $_SESSION['usuario']; ?>!</h1>
+
+<h1>Bem-vindo, <?php echo $_SESSION['usuario']; ?>!</h1>
     <a href="logout.php">Sair</a>
+
+    <header>
+        <nav id="navbar">
+            <i class="fa-solid fa-burger" id="nav_logo">food</i>
+
+            <ul id="nav_list">
+                <li>
+                    <a href="#home">Início</a>
+                </li>
+                <li>
+                    <a href="#menu">Cardápio</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Avaliações</a>
+                </li>
+            </ul>
+
+            <button class="btn-default">
+                Peça aqui
+            </button>
+            <button id="mobiles_btn">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+        </nav>
+
+        <div id="mobile_menu">
+            <ul id="mobile_nav_list">
+                <li>
+                    <a href="#home">Início</a>
+                </li>
+                <li>
+                    <a href="#menu">Cardápio</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Avaliações</a>
+                </li>
+            </ul>
+
+            <button class="btn-default">
+                Peça aqui
+            </button>
+        </div>
+    </header>
+
 </body>
 </html>
